cbfe304e1f271dd49c239624f056298e04605c1a
frbec <fredrikbeckius@gmail.com>
2018-06-26 17:02:18 +0200

    Add printBoard function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -31,3 +31,14 @@ const generateBombBoard = (numberOfRows, numberOfcolumns, numberOfBombs) => {
 
   return board;
 };
+
+const printBoard = board => console.log(board.map(row => row.join(' | ')).join('\n'));
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player board:');
+printBoard(playerBoard);
+
+console.log('Bomb board:');
+printBoard(bombBoard);
